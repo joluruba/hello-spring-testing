a19d32d5d84ebbe554a23e0be8eb34a08652f653
José Luis RB <ruizbaenajoseluis@gmail.com>
2021-08-04 15:32:17 +0200

    Configuramos que SonarQube no mire el código

---
+++ src/main/java/com/example/hellospring/HelloSpringApplication.java
@@ -0,0 +1,37 @@
+package com.example.hellospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+class HelloSpringApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(HelloSpringApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String root(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+
+    @RequestMapping("/add")
+    public @ResponseBody Float add(
+           @RequestParam(name="a") Float a,
+           @RequestParam(name = "b") Float b
+    ) {
+        return a + b;
+    }
+
+}
